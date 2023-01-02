fb72a159bb1907547b7dc22ce7b67789aea31fbb
Tiago C Fortunato <120049822+TiagoCarvalhoFortunato@users.noreply.github.com>
2023-01-02 09:52:52 -0300

    estudando

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var res = document.getElementById('res')
 
 var fotocarol = document.getElementById('fcarol')
 var fototiago = document.getElementById('ftiago')
+var fototaina = document.getElementById('ftainara')
+var fotovini = document.getElementById('fvini')
 
 
 function escolha() {
@@ -17,22 +19,53 @@ function escolha() {
 
     function ca(){
         if (
-            fotocarol.style.display == 'block' || fototiago.style.display == 'block') {
-                fotocarol.style.display = 'none'
+            fotocarol.style.display == 'none' || fototiago.style.display == 'block' || fototaina.style.display == 'block' || fotovini.style.display == 'block') {
+                fotocarol.style.display = 'block'
                 fototiago.style.display = 'none'
+                fototaina.style.display = 'none'
+                fotovini.style.display = 'none'
 
             } else {
-             fotocarol.style.display = 'block'
+             fotocarol.style.display = 'none'
             }
     }
 
-        function ti(){
-            if (
-                fototiago.style.display == 'block') {
-                    fototiago.style.display = 'none'
-                } else {
-                    fototiago.style.display = 'block'
-                }
+    function ti(){
+        if (
+            fotocarol.style.display == 'block' || fototiago.style.display == 'none' || fototaina.style.display == 'block' || fotovini.style.display == 'block') {
+                fotocarol.style.display = 'none'
+                fototiago.style.display = 'block'
+                fototaina.style.display = 'none'
+                fotovini.style.display = 'none'
 
+            } else {
+                fototiago.style.display = 'none'
+            }
+
+    }
+    function ta(){
+        if (
+            fotocarol.style.display == 'block' || fototaina.style.display == 'none' || fototiago.style.display == 'block' || fotovini.style.display == 'block') {
+                fotocarol.style.display = 'none'
+                fototaina.style.display = 'block'
+                fototiago.style.display = 'none'
+                fotovini.style.display = 'none'
+
+            } else {
+                fototaina.style.display = 'none'
+            }
+    }
+
+    function vi() {
+        if (
+            fotocarol.style.display == 'block' || fototaina.style.display == 'block' || fototiago.style.display == 'block' || fotovini.style.display == 'none') {
+                fotocarol.style.display = 'none'
+                fototaina.style.display = 'none'
+                fototiago.style.display = 'none'
+                fotovini.style.display = 'block'
+
+            } else {
+                fotovini.style.display = 'none'
+            }
+    }
     
-}
