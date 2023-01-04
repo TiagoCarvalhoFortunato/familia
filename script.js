74e1b3c56222b0ee77ad9618e9ec0f8a7bcab5b3
Tiago C Fortunato <120049822+TiagoCarvalhoFortunato@users.noreply.github.com>
2023-01-03 23:46:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,6 @@ t = 0
 tain = 0
 v = 0
 
-window.alert ('Os botões inferior é para contagem de clique que cada perfil teve, para resete att a página')
-
 function escolha() {
         fotocarol.style.display = 'none'
         fototiago.style.display = 'none'
